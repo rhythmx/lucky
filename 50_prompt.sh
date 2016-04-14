03e79ed1a62ec9d4cda75161f15be1933c15a8ff
Sean Bradly <rhythmx@gmail.com>
2016-04-14 16:40:30 -0500

    Update prompt a bit

--- 50_prompt.sh
+++ 50_prompt.sh
@@ -1,9 +1,75 @@
 # Setup a snazzy dynamic prompt
 
+PS1='[\u@\h \W]\$ ' # <== not so snazzy default
+
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-PS1='[\u@\h \W]\$ ' # <== not so snazzy default
+
+global_err=''
+
+# Dynamically build a Bash Prompt (i.e, result stored in PS1)
+function prompt_builder() {
+    
+    # Must come first, save last exit code for later use. Any other
+    # commands run will overwrite it.
+    global_err=$?
+
+    # Clear the slate
+    PS1=''
+
+    # Prompt is divided into two lines, a status line and a simple prompt
+    
+    PS1+="\n$(prompt_statusline)\n\n$(prompt_promptline)"
+
+}
+
+function prompt_promptline() {
+    echo "[\u@\h] \$ "
+}
+
+function prompt_statusline() {
+
+    time=$(date +"%H:%M %a")
+    wdir=$(pwd)
+    gitp=$(prompt_git)
+    len=$(expr length "${wdir} ${gitp}")
+    len=$((len+6))
+    line=$(printf '═%.s' $(seq 1 $len))
+    top="${bldblk}╔═${txtcyn}[${bldcyn}${time}${txtcyn}]${bldblk}$line╗"
+    bot="${bldblk}╚═════════════${txtgrn}[ ${bldgrn}${wdir} ${bldylw}${gitp}${bldgrn} ${txtgrn}]${bldblk}═╝${txtrst}" 
+    
+    if [[ $global_err == 0 ]]; then
+	c=${bldgrn}
+    else
+	c=${bldred}
+	err=" ${bldred}(failed with code=${bldylw}${bakred}${global_err}${txtrst}${bldred})"
+    fi
+
+    # echo "${c}-=<[ $(prompt_dir)${err}${bldcyn}$(__git_ps1)${c} ]>=-${txtrst}"
+    echo $top
+    echo $bot
+}
+
+function prompt_dir() {
+    echo "${bldylw}$(pwd)"
+}
+
+function prompt_git() {
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWSTASHSTATE=1
+    GIT_PS1_SHOWUNTRACKEDFILES=1
+    # Explicitly unset color (default anyhow). Use 1 to set it.
+    GIT_PS1_SHOWCOLORHINTS=1
+    GIT_PS1_DESCRIBE_STYLE="branch"
+    GIT_PS1_SHOWUPSTREAM="auto git"
+
+    echo $(__git_ps1)
+}
+# ╔═(00:46 Thu)══════════════════════════════╗
+# ╚═══════════ ~/code/lim/cxxlean (master<) ═╝
+#     
+# sean@scruffy $
 
 # Define this empty here to avoid not found errors if git is not present
 function __git_ps1() {
@@ -17,44 +83,5 @@ fi
 
 # [/path/to/full/dir GITINFO(info)]
 
-global_err=''
+PROMPT_COMMAND='prompt_builder'
 
-function prompt_init() {
-    # Must come first!
-    global_err=$?
-
-    # Clear the slate
-    PS1=''
-
-
-    # Directory info
-    PS1+=$(prompt_statusline)
-    PS1+="\n"
-
-    PS1+="${bldgrn}\u@\h${bldblu} (\A)\$ ${txtrst}"
-}
-
-function prompt_statusline() {
-    if [[ $global_err == 0 ]]; then
-	c=${bldgrn}
-    else
-	c=${bldred}
-	err=" ${bldred}(failed with code=${bldylw}${bakred}${?}${txtrst}${bldred})"
-    fi
-
-    GIT_PS1_SHOWDIRTYSTATE=1
-    GIT_PS1_SHOWSTASHSTATE=1
-    GIT_PS1_SHOWUNTRACKEDFILES=1
-    # Explicitly unset color (default anyhow). Use 1 to set it.
-    GIT_PS1_SHOWCOLORHINTS=1
-    GIT_PS1_DESCRIBE_STYLE="branch"
-    GIT_PS1_SHOWUPSTREAM="auto git"
-
-    echo "${c}-=<[ $(prompt_dir)${err}${bldcyn}$(__git_ps1)${c} ]>=-${txtrst}"
-}
-
-function prompt_dir() {
-    echo "${bldylw}$(pwd)"
-}
-
-PROMPT_COMMAND='prompt_init'
