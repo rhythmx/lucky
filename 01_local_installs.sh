f5677c32f601cf4d618792c4282b9ea62bb52877
Sean Bradly <rhythmx@gmail.com>
2016-05-02 22:13:04 -0500

    rename path to be more descripive of what it actually does

---
+++ 01_local_installs.sh
@@ -0,0 +1,6 @@
+# Stuff installed per user should prefix itself here
+PATH=$PATH:/home/sean/local/bin
+export PATH
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sean/local/lib
+export LD_LIBRARY_PATH
--- 01_path.sh
+++
@@ -1,2 +0,0 @@
-PATH=$PATH:/home/sean/local/bin
-export PATH
