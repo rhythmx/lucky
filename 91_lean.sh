836dbf77a036aaa9f6e23e3da785277ea397a42b
Sean Bradly <rhythmx@gmail.com>
2016-05-02 22:12:26 -0500

    setup lean path

--- 91_lean.sh
+++ 91_lean.sh
@@ -1,4 +1,13 @@
-if [ -d ${HOME}/code/lean/bin ]
+# nvm, don't include directly from builds any more
+#if [ -d ${HOME}/code/lean/bin ]
+#then
+#    PATH=$PATH:${HOME}/code/lean/bin
+#fi
+
+LEAN_PATH="/home/sean/code/lim/cxxlean/lean/src:/home/sean/code/lim/cxxlean/lean/src/cxx/ast"
+if which lean >/dev/null 2>&1
 then
-    PATH=$PATH:${HOME}/code/lean/bin
+	LEAN_PATH=`lean --path`:$LEAN_PATH
 fi
+
+export LEAN_PATH
