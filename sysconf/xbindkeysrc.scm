5e6d29c94815666b95dfc6c5599ff1154fb87a2d
Sean Bradly <rhythmx@gmail.com>
2016-05-04 20:36:35 -0500

    add my fn keybindings to src control

---
+++ sysconf/xbindkeysrc.scm
@@ -0,0 +1,5 @@
+(xbindkey '("XF86AudioRaiseVolume")  "amixer set Master 3%+ unmute")
+(xbindkey '("XF86AudioLowerVolume")  "amixer set Master 3%- unmute")
+(xbindkey '("XF86AudioMute")         "amixer set Master toggle")
+(xbindkey '("XF86MonBrightnessUp")   "xbacklight -inc 5")
+(xbindkey '("XF86MonBrightnessDown") "xbacklight -dec 5")
