e9bb2af671a562854198daba1ac898d9617a7dff
Sean Bradly <seanb@atredis.com>
2019-11-20 18:13:13 -0600

    Warn and add command to autoinstall if rvm is not present

--- mods-available/95_rvm.sh
+++ mods-available/95_rvm.sh
@@ -4,4 +4,16 @@ if [ -f "$HOME/.rvm/scripts/rvm" ]
 then
 	  source "$HOME/.rvm/scripts/rvm"
     export PATH="$PATH:$HOME/.rvm/bin"
+else
+    logmsg rvm::warn RVM is not installed. You can do this automatically by running lucky_rvm_install
+
+    function lucky_rvm_install() {
+        # TODO: is any special handling needed for other platforms?
+        logmsg rvm::info Installing rvm via web
+        gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+        curl -sSL https://get.rvm.io | bash -s stable
+        source "$HOME/.rvm/scripts/rvm"
+        export PATH="$PATH:$HOME/.rvm/bin"
+        rvm install 2.6
+    }
 fi
