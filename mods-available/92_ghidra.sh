13b0dd043c3b228c2742df7ff551c19bc5b085ac
Sean Bradly <seanb@atredis.com>
2022-04-24 13:21:11 -0500

    add some ghidra env config

---
+++ mods-available/92_ghidra.sh
@@ -0,0 +1,5 @@
+logmsg android::debug setting up android sdk/ndk if available
+
+if [ -d "$LUCKY_PREFIX/ghidra" ]; then
+    export GHIDRA_INSTALL_DIR="$LUCKY_PREFIX/ghidra"
+fi
---
+++ mods-enabled/92_ghidra.sh
@@ -0,0 +1,1 @@
+../mods-available/92_ghidra.sh
